d101e9552fd0a7a0ad41dc9e37e80d245287df47
Harsh Yadav <harshyadavcse@gmail.com>
2022-09-05 00:49:13 +0530

    Sorting

---
+++ insertionsort.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int insertionsort(int arr[],int n){
+        for(int i=1;i<n;i++){
+            int current =arr[i];
+            int prev= i-1;
+            while(prev>=0 and current<arr[prev]){
+                arr[prev+1]=arr[prev];
+                prev=prev-1;
+            }
+            arr[prev+1]=current;
+
+        }
+}
+int main(){
+    int arr[]={-2,3,4,-1,5,-12,6,1,3};
+    int n=sizeof(arr)/sizeof(int);
+    insertionsort(arr,n);
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
